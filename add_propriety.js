1d186ff311f15d0566078d39c0ccea3b659602a5
Denise <denise.goldan@gmail.com>
2017-04-09 15:07:33 +0300

    Added files related to the "Add property Form"
    Created a php file that includes some of the fields required to add a
    property and another file that includes the scripts used to interact
    with Google API.

---
+++ add_propriety.js
@@ -0,0 +1,24 @@
+<!--
+function initialize(){
+	initializeMap();
+	initializeAutocomplete()
+}
+function initializeMap() {
+	var mapProp= {
+		center:new google.maps.LatLng(51.508742,-0.120850),
+		zoom:5,
+	};
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+function initializeAutocomplete() {
+	// Create the autocomplete object, restricting the search to geographical location types.
+	autocomplete = new google.maps.places.Autocomplete(
+		/** @type {!HTMLInputElement} */(document.getElementById('city')),
+		{types: ['geocode']});
+}
+function recenterMap(){
+	
+	console.log("dasdasdasda")
+}
+-->
