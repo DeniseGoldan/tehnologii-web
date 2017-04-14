3a22640e351a8b4bf61437f120a684eabcbc29f9
Andrei Harpa <codrin.harpa@info.uaic.ro>
2017-04-14 15:37:28 +0300

    More javascript
    Made the page recenter and added latitude and longitude to the form

--- PerfPlaceFinder/add_property.php
+++ PerfPlaceFinder/add_property.php
@@ -9,20 +9,29 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="add_propriety.js"></script>
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRGPzmuWJ-rKRoTIZnJcNMrfdfmYwg7XQ&libraries=places&callback=initialize" async defer></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRGPzmuWJ-rKRoTIZnJcNMrfdfmYwg7XQ&libraries=places&callback=initMap" async defer></script>
+    <script type="text/javascript" src="add_propriety.js"></script>
 </head>
 <body>
 	<div class="container">
-		<form class="well" method="post">
+		<form class="well" actionmethod="get" id="addProperty">
 			<!--Define the location of the property-->
 			<fieldset>
 				<legend>Location</legend>
+
 				<div class="input-group required">
 					<span class="input-group-addon"><i class="glyphicon glyphicon-map-marker"></i></span>
-					<input name="search" placeholder="Enter a city" id="city" class="form-control" type="text">
+					<input name="search" placeholder="Enter a city" id="pac-input" class="form-control" type="text">
 				</div>
 				<div id="googleMap" style="width:100%;height:400px;"></div>
+                <div id="infowindow-content">
+                    <span id="place-name"  class="title"></span><br>
+                    <span id="place-address"></span>
+                </div>
+                <div name="LatLng">
+                    <input name="lat" type ="hidden" id="lat">
+                    <input name="lng" type ="hidden" id="lng">
+                </div>
 			</fieldset>
 			<br>
 			<!--Select the type of property-->
@@ -79,15 +88,13 @@
 				</div>
 			</fieldset>
 			<!--Submit data for further validation-->
-			<button type="submit" name="submitButton">Submit</button>
+			<button type="submit" name="submitButton" ;">Submit</button>
 		</form>
 	</div>
-
-<script>
-document.getElementById('city').addEventListener('submit',recenterMap)
-</script>
+    <script>
 
 
+    </script>
 
 
 	</body>
--- PerfPlaceFinder/add_propriety.js
+++ PerfPlaceFinder/add_propriety.js
@@ -1,24 +1,74 @@
-<!--
-function initialize(){
-	initializeMap();
-	initializeAutocomplete()
-}
-function initializeMap() {
-	var mapProp= {
-		center:new google.maps.LatLng(51.508742,-0.120850),
-		zoom:5,
-	};
-	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var marker;
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('googleMap'), {
+        center: {lat: -33.8688, lng: 151.2195},
+        zoom: 13
+    });
+    var input = document.getElementById('pac-input');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', map);
+
+    var infowindow = new google.maps.InfoWindow();
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+    marker = new google.maps.Marker({
+        map: map,
+        anchorPoint: new google.maps.Point(0, -29),
+		draggable:true
+    });
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+        }
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+            address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+        }
+        infowindowContent.children['place-name'].textContent = place.name;
+        infowindowContent.children['place-address'].textContent = address;
+        infowindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker, 'dragend', function (event) {
+        document.getElementById("lat").value = event.latLng.lat().toFixed(3);
+        document.getElementById("lng").value = event.latLng.lng().toFixed(3);
+        console.log("papanasi");
+    });
+    // document.getElementById('use-strict-bounds')
+    //     .addEventListener('click', function() {
+    //         console.log('Checkbox clicked! New state=' + this.checked);
+    //         autocomplete.setOptions({strictBounds: this.checked});
+    //     });
+
+
 }
 
-function initializeAutocomplete() {
-	// Create the autocomplete object, restricting the search to geographical location types.
-	autocomplete = new google.maps.places.Autocomplete(
-		/** @type {!HTMLInputElement} */(document.getElementById('city')),
-		{types: ['geocode']});
-}
-function recenterMap(){
 
-	console.log("dasdasdasda")
-}
--->
