6c8a9bdc45a895cf22fb4a5d81cad494c5ee415f
Andrei Harpa <codrin.harpa@info.uaic.ro>
2017-05-23 19:04:39 +0300

    +

--- laravel/perfplace/app/House.php
+++ laravel/perfplace/app/House.php
@@ -5,4 +5,5 @@ namespace App;
 use Jenssegers\Mongodb\Eloquent\Model;
 class House extends Model
 {
+   protected $fillable = ['id','userId','title','description','numberOfRooms','surface','price','transactionType','latitude','longitude','country','city','address','numberOfFloors'];
 }
